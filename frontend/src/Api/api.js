73fd80b1ea883bc5e651d30356bc8ecf8aa5d745
Sagar <sagarjha2004@gmail.com>
2025-10-03 08:22:12 +0530

    updated env

--- frontend/src/Api/api.js
+++ frontend/src/Api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "../utils/cookies";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:7001/api";
+  import.meta.env.VITE_API_URL || "http://localhost:7001/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
