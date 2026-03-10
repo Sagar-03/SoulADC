f3980576cddac3b7b368d1f719fe9f7f4dabf5f8
Sagar <sagarjha2004@gmail.com>
2026-03-11 00:36:19 +0530

    preloader commented

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -10,23 +10,23 @@ import Highlights from "../Components/HomePage/Highlights/Highlights";
 import CourseCurriculum from "../Components/HomePage/Coursecurriulam/CourseCurriculum";
 import Board from "../Components/HomePage/Board/Board";
 import MentorCard from "../Components/HomePage/Mentor/MentorCard";
-import Preloader from "../Components/HomePage/PreLoader/Preloader";
+// import Preloader from "../Components/HomePage/PreLoader/Preloader";
 
 
 // --- Homepage Component ---
 const HomePage = () => {
-  const [loadingDone, setLoadingDone] = useState(false);
+  // const [loadingDone, setLoadingDone] = useState(false);
   const [showAuth, setShowAuth] = useState(false);
-  
+
   // Memoize callbacks to prevent unnecessary re-renders
-  const handlePreloaderFinish = useCallback(() => setLoadingDone(true), []);
+  // const handlePreloaderFinish = useCallback(() => setLoadingDone(true), []);
   const handleSignInClick = useCallback(() => setShowAuth(true), []);
   const handleAuthClose = useCallback(() => setShowAuth(false), []);
-  
+
   return (
     <>
-      {!loadingDone && <Preloader onFinish={handlePreloaderFinish} />}
-      {loadingDone && (
+      {/* {!loadingDone && <Preloader onFinish={handlePreloaderFinish} />} */}
+      {/* {loadingDone && ( */}
         <div className="main-content fade-in">
           <Navbar onSignInClick={handleSignInClick} />
           <Registration />
@@ -39,7 +39,7 @@ const HomePage = () => {
           <Footer />
           {showAuth && <Auth onClose={handleAuthClose} />}
         </div>
-      )}
+      {/* )} */}
     </>
   );
 };
